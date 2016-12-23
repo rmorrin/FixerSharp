55a9b4d0f5656dcd516813449b18625473c8da6a
Ryan Morrin <ryanmorrin@gmail.com>
2016-12-23 09:42:35 +0000

    initial work on async support. still need tests adding.

--- FixerSharp/Fixer.cs
+++ FixerSharp/Fixer.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace FixerSharp
 {
@@ -13,9 +14,19 @@ namespace FixerSharp
             return GetRate(from, to, date).Convert(amount);
         }
 
+        public static async Task<double> ConvertAsync(string from, string to, double amount, DateTime? date = null)
+        {
+            return (await GetRateAsync(from, to, date)).Convert(amount);
+        }
+
         public static ExchangeRate Rate(string from, string to, DateTime? date = null)
         {
             return GetRate(from, to, date);
+        }
+
+        public static async Task<ExchangeRate> RateAsync(string from, string to, DateTime? date = null)
+        {
+            return await GetRateAsync(from, to, date);
         }
 
         private static ExchangeRate GetRate(string from, string to, DateTime? date = null)
@@ -30,10 +41,42 @@ namespace FixerSharp
                 throw new ArgumentException("Symbol not found for provided currency", "to");
 
             // Get data from Fixer API
-            var fixerData = GetFixerData(date);
+            var dateString = date.HasValue ? date.Value.ToString("yyyy-MM-dd") : "latest";
+            var url = string.Format("{0}{1}", BaseUri, dateString);
 
-            // Parse rates
-            var rates = fixerData.Value<JObject>("rates");
+            using (var wc = new WebClient())
+                return ParseData(wc.DownloadString(url), from, to);
+        }
+
+        private static async Task<ExchangeRate> GetRateAsync(string from, string to, DateTime? date = null)
+        {
+            from = from.ToUpper();
+            to = to.ToUpper();
+
+            if (!Symbols.IsValid(from))
+                throw new ArgumentException("Symbol not found for provided currency", "from");
+
+            if (!Symbols.IsValid(to))
+                throw new ArgumentException("Symbol not found for provided currency", "to");
+
+            // Get data from Fixer API
+            var dateString = date.HasValue ? date.Value.ToString("yyyy-MM-dd") : "latest";
+            var url = string.Format("{0}{1}", BaseUri, dateString);
+
+            using (var wc = new WebClient())
+                return ParseData(await wc.DownloadStringTaskAsync(url), from, to);
+        }
+
+        private static ExchangeRate ParseData(string data, string from, string to)
+        {
+            // Parse JSON
+            var root = JObject.Parse(data);
+
+            // Exchange base is not included in rates, add this manually
+            var rates = root.Value<JObject>("rates");
+            var baseSymbol = root.Value<string>("base");
+            rates.Add(baseSymbol, 1.00);
+
             var fromRate = rates.Value<double>(from);
             var toRate = rates.Value<double>(to);
 
@@ -41,28 +84,10 @@ namespace FixerSharp
 
             // Parse returned date
             // Note: This may be different to the requested date as Fixer will return the closest available
-            var returnedDate = DateTime.ParseExact(fixerData.Value<string>("date"), "yyyy-MM-dd",
+            var returnedDate = DateTime.ParseExact(root.Value<string>("date"), "yyyy-MM-dd",
                 System.Globalization.CultureInfo.InvariantCulture);
 
             return new ExchangeRate(from, to, rate, returnedDate);
-        }
-
-        private static JObject GetFixerData(DateTime? date = null)
-        {
-            var dateString = date.HasValue ? date.Value.ToString("yyyy-MM-dd") : "latest";
-            var url = string.Format("{0}{1}", BaseUri, dateString);
-
-            using (var wc = new WebClient())
-            {
-                var root = JObject.Parse(wc.DownloadString(url));
-                var rates = root.Value<JObject>("rates");
-
-                // exchange base is not included in rates, add this manually
-                var baseSymbol = root.Value<string>("base");
-                rates.Add(baseSymbol, 1.00);
-
-                return root;
-            }
         }
     }
 }
